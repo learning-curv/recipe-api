e2e052addbf74d6315a006eec60f6a8023ad25da
Ruben <rubendevilliers01@gmail.com>
2023-04-03 15:10:12 +0200

    finished recipe type and error checks

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
-
-from typing import Union, List
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, parse_obj_as, ValidationError
 import requests
 
 
 class Token(BaseModel):
-    type: int
-    name: str
+    type: int | None = None
+    name: str | None = None
 
 
 class Messure(BaseModel):
@@ -26,12 +24,12 @@ class Ingredients(BaseModel):
 
 class IngredientGroup(BaseModel):
     title: str | None = None
-    ingredients: List[Ingredients]
+    ingredients: list[Ingredients]
 
 
 class Instruction(BaseModel):
     title: str | None = None
-    instructions: List[str]
+    instructions: list[str]
 
 
 class Recipe(BaseModel):
@@ -42,8 +40,8 @@ class Recipe(BaseModel):
     cook_time: Messure
     prep_time: Messure
     description: str
-    ingredient_groups: List[IngredientGroup]
-    instruction_groups: List[Instruction]
+    ingredient_groups: list[IngredientGroup]
+    instruction_groups: list[Instruction]
 
 
 # id          | bfa6d474-57e4-4878-93c2-7ad31933f32b
@@ -68,14 +66,92 @@ def hello_world():
 
 
 @app.get("/api/recipe/{recipe_id}")
-def token(recipe_id: str, q: Union[str, None] = None):
+async def token(recipe_id: str, q: str | None = None):
     # postgres select where id = recipe_id
     # return recipe_data
-    pass
+    api_data = {
+        "link": "https://www.thechunkychef.com/spiced-soft-molasses-cookies/",
+        "title": "Spiced Soft Molasses Cookies",
+        "author": None,
+        "servings": "24",
+        "cook_time": {"time": "15", "unit": "minutes"},
+        "prep_time": {"time": "10", "unit": "minutes"},
+        "description": "Soft and perfectly spiced, these cookies will soon become one of your favorite! These cookies are soft and chewy, with that classic dark molasses flavor!",
+        "ingredient_groups": [
+            {
+                "title": None,
+                "ingredients": [
+                    {
+                        "name": "packed brown sugar",
+                        "unit": "cup",
+                        "notes": None,
+                        "amount": "1",
+                    },
+                    {
+                        "name": "vegetable shortening",
+                        "unit": "cup",
+                        "notes": None,
+                        "amount": "3/4",
+                    },
+                    {"name": "molasses", "unit": "cup", "notes": None, "amount": "1/4"},
+                    {"name": "egg", "unit": None, "notes": None, "amount": "1"},
+                    {
+                        "name": "all purpose flour",
+                        "unit": "cups",
+                        "notes": None,
+                        "amount": "2 1/4",
+                    },
+                    {
+                        "name": "baking soda",
+                        "unit": "tsp",
+                        "notes": None,
+                        "amount": "2",
+                    },
+                    {
+                        "name": "ground cinnamon",
+                        "unit": "tsp",
+                        "notes": None,
+                        "amount": "1",
+                    },
+                    {
+                        "name": "ground ginger",
+                        "unit": "tsp",
+                        "notes": None,
+                        "amount": "1",
+                    },
+                    {
+                        "name": "ground cloves",
+                        "unit": "tsp",
+                        "notes": None,
+                        "amount": "1/2",
+                    },
+                    {"name": "salt", "unit": "tsp", "notes": None, "amount": "1/4"},
+                ],
+            }
+        ],
+        "instruction_groups": [
+            {
+                "title": None,
+                "instructions": [
+                    "Preheat oven to 325 degrees.",
+                    "In a large bowl, beat brown sugar, shortening, molasses, and egg until combined. Stir in remaining ingredients.",
+                    "Shape dough into rounded balls, about 1” in diameter. Place some granulated sugar in a small bowl and roll balls in the sugar.",
+                    "Place the cookie balls onto a parchment paper lined baking sheet, about 2” apart.",
+                    "Bake for 13-16 minutes until just set and cookies appear dry and cracked.",
+                    "Remove from baking sheet to cooling rack",
+                ],
+            }
+        ],
+    }
+    try:
+        return parse_obj_as(Recipe, api_data)
+
+    except ValidationError as e:
+        return e.errors()
 
 
 @app.put("/api/recipe/{recipe_id}")
-def update_recipe(recipe_id: str, recipe: Recipe):
+async def update_recipe(recipe_id: str, recipe: Recipe):
     # postgres update table for recipe_id
 
     return {
@@ -92,10 +168,15 @@ def update_recipe(recipe_id: str, recipe: Recipe):
 
 
 @app.get("/api/token/{token_id}")
-def token(token_id: int, q: Union[str, None] = None):
-    return {"token_id": token_id, "name": None, "type": None}
+async def token(token_id: int, q: str | None = None):
+    api_data = {"name": 12345, "type": "123av"}
+    try:
+        return parse_obj_as(Token, api_data)
+
+    except ValidationError as e:
+        return e.errors()
 
 
 @app.put("/api/token/{token_id}")
-def update_token(token_id: int, token: Token):
+async def update_token(token_id: int, token: Token):
     return {"token_id": token_id, "name": token.name, "type": token.type}
